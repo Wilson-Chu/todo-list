d4ea9d4ac58cbe579fd6797c35eb1e42e09469d4
Wilson Chu <wilson1.chu@gmail.com>
2022-06-09 20:39:40 -0400

    setup App.js state handling

--- src/App.js
+++ src/App.js
@@ -11,14 +11,35 @@ class App extends Component {
     };
   }
 
+  handleChange = (e) => {
+    this.setState({
+      task: {
+        text: e.target.value,
+      },
+    });
+  };
+
+  onSubmitTask = (e) => {
+    e.preventDefault();
+    this.setState({
+      tasks: this.state.tasks.concat(this.state.task),
+      task: { text: "" },
+    });
+  };
+
   render() {
     const { task, tasks } = this.state;
 
     return (
       <div className="App">
-        <form>
+        <form onSubmit={this.onSubmitTask}>
           <label htmlFor="taskInput">Enter task</label>
-          <input type="text" id="taskInput" />
+          <input
+            onChange={this.handleChange}
+            value={task.text}
+            type="text"
+            id="taskInput"
+          />
           <button type="submit">Add Task</button>
         </form>
       </div>
